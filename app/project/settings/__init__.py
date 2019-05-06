bedc5331104486ed0cb4ed60e78992e3ba663135
Ididit4Ulin <uuuuiiiaaa@gmail.com>
2019-05-06 13:35:04 +0300

    add nginx and django/gunicorn

---
+++ app/project/settings/__init__.py
@@ -0,0 +1,14 @@
+import os
+try:
+    DEVELOP = os.environ.get('ENV') in ['DEV',]
+except KeyError:
+    DEVELOP = False
+
+print (DEVELOP)
+
+if DEVELOP:
+    print ('DEVELOP')
+    from .develop import *
+else:
+    print ('PRO')
+    from .prodaction import *
