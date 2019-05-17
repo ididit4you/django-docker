72818b1ad3bd935fa90dfc184cc83468abae118c
Ididit4Ulin <uuuuiiiaaa@gmail.com>
2019-05-17 17:40:06 +0300

    addded django.env file

--- app/src/project/settings/prodaction.py
+++ app/src/project/settings/prodaction.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9ktx1)^h@s6cji2m(muomfu$2f_q8_fb_j07zztv%og+r*)6c='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST_1')]
 
 
 # Application definition
